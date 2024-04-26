c6c5a53593480adcf3a5a1fea369afa37a19f850
Mohammed Nabeel <128857044+mohamadnabel@users.noreply.github.com>
2024-04-26 20:22:48 +0300

    add test for signup suite

--- src/parameters/Parameters.java
+++ src/parameters/Parameters.java
@@ -1,6 +1,9 @@
 package parameters;
 
+import java.util.Random;
+
 public class Parameters {
 	protected String URL = "https://automationteststore.com/";
+	protected Random rand = new Random();
 
 }
--- src/testPackage/Testcase.java
+++ src/testPackage/Testcase.java
@@ -2,18 +2,21 @@ package testPackage;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import parameters.Parameters;
 
 public class Testcase extends Parameters {
+	WebDriver driver = new ChromeDriver();
 
 	@BeforeTest
 	public void mySetup() {
-		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(9));
 		driver.get(URL);
@@ -22,5 +25,32 @@ public class Testcase extends Parameters {
 
 	@Test
 	public void myTest() {
+
+		WebElement createAccountBtn = driver.findElement(By.cssSelector("ul[id='customer_menu_top'] li a"));
+		createAccountBtn.click();
+		WebElement createNewAccountBtn = driver.findElement(By.cssSelector("button[title='Continue']"));
+		createNewAccountBtn.click();
+
+	}
+
+	@Test
+	public void regestrationNewAccount() {
+
+		WebElement firstNameInput = driver.findElement(By.cssSelector("#AccountFrm_firstname"));
+		WebElement lastNameInput = driver.findElement(By.cssSelector("#AccountFrm_lastname"));
+		WebElement emailInput = driver.findElement(By.cssSelector("#AccountFrm_email"));
+		WebElement addresslInput = driver.findElement(By.cssSelector("#AccountFrm_address_1"));
+		WebElement citylInput = driver.findElement(By.cssSelector("#AccountFrm_city"));
+		WebElement zoneID = driver.findElement(By.cssSelector("#AccountFrm_zone_id"));
+		WebElement postCode = driver.findElement(By.cssSelector("#AccountFrm_postcode"));
+		WebElement country = driver.findElement(By.cssSelector("#AccountFrm_country_id"));
+
+		Select selectorOne = new Select(zoneID);
+		int randomRegion = rand.nextInt(50);
+		selectorOne.selectByIndex(randomRegion);
+
+		Select selectorTow = new Select(country);
+		int randomCountry = rand.nextInt(50);
+		selectorTow.selectByIndex(randomCountry);
 	}
 }
