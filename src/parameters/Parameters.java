7cce1326eb1575318c8b90aad194252d2a942178
Mohammed Nabeel <128857044+mohamadnabel@users.noreply.github.com>
2024-04-27 18:53:45 +0300

    add suiteTest for registration

--- src/parameters/Parameters.java
+++ src/parameters/Parameters.java
@@ -5,5 +5,11 @@ import java.util.Random;
 public class Parameters {
 	protected String URL = "https://automationteststore.com/";
 	protected Random rand = new Random();
+	protected String[] randoomFirstName = { "mohammad", "ahmad", "yousef", "Esaa", "rama" };
+	protected String[] randoomLastName = { "mahmoud", "yazan", "basell", "kallel", "omran" };
+	protected int randLName = rand.nextInt(randoomLastName.length);
+	protected int randFName = rand.nextInt(randoomFirstName.length);
+	protected int randomNum = rand.nextInt(99);
+
 
 }
--- src/testPackage/Testcase.java
+++ src/testPackage/Testcase.java
@@ -1,11 +1,13 @@
 package testPackage;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -14,8 +16,9 @@ import parameters.Parameters;
 
 public class Testcase extends Parameters {
 	WebDriver driver = new ChromeDriver();
+	Actions action = new Actions(driver);
 
-	@BeforeTest
+	@BeforeTest(enabled = true)
 	public void mySetup() {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(9));
@@ -23,7 +26,7 @@ public class Testcase extends Parameters {
 
 	}
 
-	@Test
+	@Test(enabled = true)
 	public void myTest() {
 
 		WebElement createAccountBtn = driver.findElement(By.cssSelector("ul[id='customer_menu_top'] li a"));
@@ -33,8 +36,8 @@ public class Testcase extends Parameters {
 
 	}
 
-	@Test
-	public void regestrationNewAccount() {
+	@Test(enabled = true)
+	public void regestrationNewAccount() throws InterruptedException {
 
 		WebElement firstNameInput = driver.findElement(By.cssSelector("#AccountFrm_firstname"));
 		WebElement lastNameInput = driver.findElement(By.cssSelector("#AccountFrm_lastname"));
@@ -44,13 +47,45 @@ public class Testcase extends Parameters {
 		WebElement zoneID = driver.findElement(By.cssSelector("#AccountFrm_zone_id"));
 		WebElement postCode = driver.findElement(By.cssSelector("#AccountFrm_postcode"));
 		WebElement country = driver.findElement(By.cssSelector("#AccountFrm_country_id"));
+		WebElement loginUser = driver.findElement(By.cssSelector("#AccountFrm_loginname"));
+		WebElement passInput = driver.findElement(By.cssSelector("#AccountFrm_password"));
+		WebElement confirmPassInput = driver.findElement(By.cssSelector("#AccountFrm_confirm"));
+		WebElement submitBtn = driver.findElement(By.cssSelector("button[title='Continue']"));
 
+		firstNameInput.sendKeys(randoomFirstName[randFName]);
+		lastNameInput.sendKeys(randoomLastName[randLName]);
+		emailInput.sendKeys(randoomFirstName[randFName] + randoomLastName[randLName] + randomNum + "@gmail.com");
+		addresslInput.sendKeys("amman");
+		citylInput.sendKeys("jordan");
 		Select selectorOne = new Select(zoneID);
-		int randomRegion = rand.nextInt(50);
+		Thread.sleep(1500);
+		int randomRegion = rand.nextInt(20);
 		selectorOne.selectByIndex(randomRegion);
+		Select selectorO = new Select(zoneID);
+		Thread.sleep(1500);
+		selectorO.selectByIndex(randomRegion);
+		postCode.sendKeys("8974510");
+		loginUser.sendKeys(randoomFirstName[randFName] + randoomLastName[randLName]);
+		passInput.sendKeys("Mohmmad@123456");
+		confirmPassInput.sendKeys("Mohmmad@123456");
+		driver.findElement(By.cssSelector("#AccountFrm_agree")).click();
+		submitBtn.click();
+	}
 
-		Select selectorTow = new Select(country);
-		int randomCountry = rand.nextInt(50);
-		selectorTow.selectByIndex(randomCountry);
+	@Test(enabled = false)
+	public void regestrationNgewAccount() throws InterruptedException {
+		driver.get("https://leaders.jo/en/lg/");
+		driver.manage().window().maximize();
+
+		WebElement allItemCountiner = driver.findElement(By.cssSelector(
+				".products.product-card-layout-1.columns-6.mobile-col-2.mobile-show-atc.mobile-featured-icons--hover"));
+		List<WebElement> listItem = allItemCountiner.findElements(By.tagName("li"));
+		System.out.println(listItem.size());
+		int randomNumberOfItem = rand.nextInt(listItem.size());
+		action.moveToElement(listItem.get(randomNumberOfItem)).perform();
+		Thread.sleep(2000);
+		driver.findElement(By.partialLinkText("Add to")).click();
+		Thread.sleep(1000);
+
 	}
 }
